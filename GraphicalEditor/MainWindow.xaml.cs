da7201c1e82ad6c75204996f888245c26ff21cac
BSUIRHelper <yevgeny2006@gmail.com>
2025-03-18 22:16:23 +0300

    Adding simple controls to test the technology

--- GraphicalEditor/MainWindow.xaml.cs
+++ GraphicalEditor/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace GraphicalEditor
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
